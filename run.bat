89d841911f86032f4da9c37fd942915010d46449
NotYourAverageGamer <104248676+NotYourAverageGamer@users.noreply.github.com>
2024-06-01 23:21:48 +1000

    deleted:    ascensionchat.conf deleted:    run.bat deleted:    run.sh

--- run.bat
+++
@@ -1,3 +0,0 @@
-chcp 65001
-java -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar ascensionchat.jar ascensionchat.conf
-pause
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar ascensionchat.jar ascensionchat.conf
